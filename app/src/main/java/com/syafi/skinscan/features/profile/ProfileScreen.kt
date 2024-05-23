a8e906b6b602783ac657ba3dfc24066ab11471f3
daniswari <danis.yurin.dy@gmail.com>
2024-05-23 22:30:14 +0700

    add profile indentifier

--- app/src/main/java/com/syafi/skinscan/features/profile/ProfileScreen.kt
+++ app/src/main/java/com/syafi/skinscan/features/profile/ProfileScreen.kt
@@ -1,9 +1,10 @@
 package com.syafi.skinscan.features.profile
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun ProfileScreen() {
-
+    Text(text = "Profile")
 }
